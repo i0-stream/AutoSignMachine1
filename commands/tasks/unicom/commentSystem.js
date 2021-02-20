ce0b6b9e40c325be31485405961ef1b7f1719bfe
fangcaoqiqi <51883234+fangcaoqiqi@users.noreply.github.com>
2021-02-20 16:35:22 +0800

    Update commentSystem.js

--- commands/tasks/unicom/commentSystem.js
+++ commands/tasks/unicom/commentSystem.js
@@ -1,63 +1,4 @@
-var transParams = (data) => {
-  let params = new URLSearchParams();
-  for (let item in data) {
-    params.append(item, data['' + item + '']);
-  }
-  return params;
-};
-module.exports = {
-  commentTask: async (axios, options) => {
-    const useragent = `Mozilla/5.0 (Linux; Android 7.1.2; SM-G977N Build/LMY48Z; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:${options.user}};devicetype{deviceBrand:samsung,deviceModel:SM-G977N};{yw_code:}    `
-    console.log('尝试文章点赞')
-    let result = await axios.request({
-      baseURL: 'https://m.client.10010.com/',
-      headers: {
-        "user-agent": useragent,
-        "referer": "https://img.client.10010.com/kuaibao/index.html",
-        "origin": "https://img.client.10010.com"
-      },
-      url: `/commentSystem/getNewsList`,
-      method: 'post',
-      data: transParams({
-        pageNum: 1,
-        pageSize: 10,
-        reqChannel: '00'
-      })
-    }).catch(err => console.log('获取文章数据失败'))
-    if (!result || result.data.code !== '0000') {
-      throw new Error('获取文章数据失败')
-    }
-    let newsIds = result.data.data.map(i => (
-      {
-        id: i.id,
-        mainTitle: i.mainTitle,
-        subTitle: i.subTitle
-      }
-    ))
-    if (!newsIds.length) {
-      throw new Error('获取文章数据失败')
-    }
-    for (let newsId of newsIds) {
-      let result
-      try {
-        result = await axios.request({
-          baseURL: 'https://m.client.10010.com/',
-          headers: {
-            "user-agent": useragent,
-            "referer": `https://img.client.10010.com/kuaibao/detail.html?pageFrom=newsList&id=${newsId.id}&taskId=`,
-            "origin": "https://img.client.10010.com"
-          },
-          url: `/commentSystem/getCommentList`,
-          method: 'post',
-          data: transParams({
-            id: newsId.id,
-            pageNum: 1,
-            pageSize: 20,
-            reqChannel: 'quickNews'
-          })
-        })
-      } catch (err) {
-        continue
+continue
       }
       let data = result.data
       if (!result || data.code !== '0000') {
@@ -138,7 +79,7 @@ module.exports = {
             'reqChannel': 'quickNews',
             'reqId': newsId.id
           })
-        }).catch(() => console.log('文章点赞失败'))
+         }).catch(() => console.log('文章点赞失败'))
         // 进行点赞
         let result = await axios.request({
           baseURL: 'https://m.client.10010.com/',
